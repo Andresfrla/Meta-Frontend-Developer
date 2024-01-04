b261dd3bafe00a91416fc1647a9320636f231169
Andres Franco <andresfranla@gmail.com>
2024-01-04 17:57:48 -0600

    Booking done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Nav from './components/Nav';
-import Header from './components/Header';
+import Main from './components/Main';
 
 function App() {
   return (
     <>
       <Nav />
-      <Header />
+      <Main />
     </>
   );
 }
